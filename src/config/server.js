2f70656ab0b9847ebffbe67fa0422d6b3ad0724c
leoncoutinho1 <leon.coutinho1@hotmail.com>
2020-08-21 21:09:22 -0300

    Modify point of define port

--- src/config/server.js
+++ src/config/server.js
@@ -1,10 +1,10 @@
-const port = process.env.PORT || 3003;
-
 require('dotenv/config')
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const cors = require('./cors')
+
+const port = process.env.PORT || 3003;
 
 server.use(cors)
 server.use(bodyParser.urlencoded({ extended: true }))
